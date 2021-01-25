3ad3140d3305ad6b728e4a4164884f798a71b296
Philip Davis <philipcdavis11@gmail.com>
2021-01-25 12:27:42 -0500

    Initial Commit

---
+++ UnsplashAPI/UnsplashAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAPIApp.swift
+//  UnsplashAPI
+//
+//  Created by Philip Davis on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
